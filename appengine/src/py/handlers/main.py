1a3ab24707f7ce68c0b207d9f6f2e70dca99e277
Riccardo Govoni <battlehorse@gmail.com>
2010-07-05 20:31:17 +0200

    First rough moving of intro material away from core rhizosphere js code

---
+++ appengine/src/py/handlers/main.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import os.path
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from py import rhizoglobals
+
+class MainHandler(webapp.RequestHandler):
+
+    def get(self):
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__), '../../templates/index.html')
+        self.response.out.write(template.render(path, template_values))
+
+application = webapp.WSGIApplication(
+    [('/', MainHandler),],
+    debug=rhizoglobals.debug)
+
+
+def main():
+    run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
