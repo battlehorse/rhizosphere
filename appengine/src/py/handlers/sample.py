3882c9c612b5cfa12fd1c99a21e0f67885fbaac1
Riccardo Govoni <battlehorse@google.com>
2010-07-09 13:09:08 +0100

    Refactored samples into a separate directory. Removed legacy non-appengine rhizo.html

--- appengine/src/py/handlers/sample.py
+++ appengine/src/py/handlers/sample.py
@@ -1,1 +1,31 @@
 #!/usr/bin/env python
+
+import os.path
+
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp import template
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from py import rhizoglobals
+
+class ScriptHandler(webapp.RequestHandler):
+    def get(self):
+        jsfile = self.request.path.replace('/sample/', '')
+        template_values = {}
+        path = os.path.join(os.path.dirname(__file__),
+                            '../../static/samples/js/%s' % jsfile)
+        self.response.headers.add_header('Content-Type', 'text/javascript')
+        self.response.out.write(template.render(path, template_values))
+
+
+application = webapp.WSGIApplication(
+    [('/sample/.*', ScriptHandler),],
+    debug=rhizoglobals.debug)
+
+
+def main():
+    run_wsgi_app(application)
+
+
+if __name__ == '__main__':
+    main()
