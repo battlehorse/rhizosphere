2d603ace2cc47f91a34c417843401c36b53668ca
Riccardo Govoni <battlehorse@google.com>
2010-07-20 11:09:46 +0100

    Using anonymous functions in samples to avoid polluting the global namespace

--- samples/js/books.js
+++ samples/js/books.js
@@ -14,160 +14,163 @@
   limitations under the License.
 */
 
-namespace("sample");
+(function() {
+  var Book = function(isbn, title, img, rating, price, genres, paperback) {
+    this.id = isbn;
+    this.isbn = isbn;
+    this.title = title;
+    this.img = img;
+    this.rating = rating;
+    this.price = price;
+    this.genres = genres;
+    this.paperback = paperback;
+  };
 
-sample.Book = function(isbn, title, img, rating, price, genres, paperback) {
-  this.id = isbn;
-  this.isbn = isbn;
-  this.title = title;
-  this.img = img;
-  this.rating = rating;
-  this.price = price;
-  this.genres = genres;
-  this.paperback = paperback;
-};
+  Book.prototype.toString = function() {
+    return this.title + "(" + this.isbn + ")";
+  };
 
-sample.Book.prototype.toString = function() {
-  return this.title + "(" + this.isbn + ")";
-};
+  var models = [
+    new Book('1590595009',
+             'Writing Scientific Software',
+             'http://ecx.images-amazon.com/images/I/41PSP75PJFL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
+             4,
+             16.49,
+             ['programming', 'manual'],
+             true),
+    new Book('1590593898',
+             'Joel On Software',
+             'http://ecx.images-amazon.com/images/I/51MeZFORwWL._SL160_AA115_.jpg',
+             3,
+             18.20,
+             ['programming', 'history'],
+             false),
+    new Book('0321525655',
+             'Presentation Zen',
+             'http://ecx.images-amazon.com/images/I/41hMmcWQdwL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
+             4,
+             65.79,
+             ['programming'],
+             true),
+    new Book('0340284028',
+             'Swallowing Darkness',
+             'http://ecx.images-amazon.com/images/I/51iOGCvzybL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
+             5,
+             25,
+             [ 'crime'],
+             false),
+    new Book('8938928494',
+             'Gone With the Wind',
+             'http://ecx.images-amazon.com/images/I/51vjRjG5UxL._SL160_AA115_.jpg',
+             2,
+             27,
+             [ 'romance'],
+             false),
+    new Book('382822833',
+             'Art: A world History',
+             'http://ecx.images-amazon.com/images/I/514dfpLu5GL._SL160_AA115_.jpg',
+             3,
+             95,
+             [ 'manual'],
+             false),
+    new Book('84823221177',
+             "The Sagan Diary",
+             'http://ecx.images-amazon.com/images/I/51FlsY9OKVL._SL160_AA115_.jpg',
+             5,
+             10.3,
+             [ 'sci-fi', 'crime'],
+             true),
+    new Book('14823221177',
+             "The Last Patrion",
+             'http://ecx.images-amazon.com/images/I/51de0isP05L._SL500_SL150_.jpg',
+             1,
+             43,
+             [ 'politics', 'crime'],
+             true),
+    new Book('84823421177',
+             "Brisingr (Inheritance)",
+             'http://ecx.images-amazon.com/images/I/51ynF4PIThL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
+             2,
+             72,
+             [ 'fantasy', 'romance'],
+             false),
+    new Book('84823226677',
+             "Rediscover God in America",
+             'http://ecx.images-amazon.com/images/I/41VR8ZKNKKL._SL500_SL150_.jpg',
+             4,
+             35,
+             [ 'politics'],
+             true),
+    new Book('84823221187',
+             "The Digital Photography Book",
+             'http://ecx.images-amazon.com/images/I/4127fI6pghL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
+             4,
+             24.3,
+             [ 'manual'],
+             true),
+    new Book('848000000',
+             "Apple Aperture 2",
+             'http://ecx.images-amazon.com/images/I/51PmGTgxJ6L._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
+             3,
+             43,
+             [ 'programming', 'manual'],
+             false),
+    new Book('848232890077',
+             "Anne Frank: The Diary",
+             'http://ecx.images-amazon.com/images/I/519HKX9M69L._SL500_SL150_.jpg',
+             5,
+             10.3,
+             [ 'history'],
+             true)
+  ];
 
+  var metamodel = {
+    isbn: { kind: rhizo.meta.Kind.STRING, label: "ISBN" },
+    title: { kind: rhizo.meta.Kind.STRING, label: "Title" },  
+    rating: { kind: rhizo.meta.Kind.RANGE, label: "Rating",
+              min: 1, max: 5, stepping: 1},
+    price: { kind: rhizo.meta.Kind.RANGE, label: "Price", min: 1, max: 100},
+    paperback: { kind: rhizo.meta.Kind.BOOLEAN, label: "Paperback" },
+    genres: { kind: rhizo.meta.Kind.CATEGORY, label: "Genres" , 
+              categories: [ 'sci-fi', 'fantasy', 'crime' , 'politics',
+                            'fiction', 'romance', 'programming', 'manual',
+                            'history'] , 
+              multiple: true}
+  };
 
-var models = [
-  new sample.Book('1590595009',
-                  'Writing Scientific Software',
-                  'http://ecx.images-amazon.com/images/I/41PSP75PJFL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
-                  4,
-                  16.49,
-                  ['programming', 'manual'],
-                  true),
-  new sample.Book('1590593898',
-                  'Joel On Software',
-                  'http://ecx.images-amazon.com/images/I/51MeZFORwWL._SL160_AA115_.jpg',
-                  3,
-                  18.20,
-                  ['programming', 'history'],
-                  false),
-  new sample.Book('0321525655',
-                  'Presentation Zen',
-                  'http://ecx.images-amazon.com/images/I/41hMmcWQdwL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
-                  4,
-                  65.79,
-                  ['programming'],
-                  true),
-  new sample.Book('0340284028',
-                  'Swallowing Darkness',
-                  'http://ecx.images-amazon.com/images/I/51iOGCvzybL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
-                  5,
-                  25,
-                  [ 'crime'],
-                  false),
-  new sample.Book('8938928494',
-                  'Gone With the Wind',
-                  'http://ecx.images-amazon.com/images/I/51vjRjG5UxL._SL160_AA115_.jpg',
-                  2,
-                  27,
-                  [ 'romance'],
-                  false),
-  new sample.Book('382822833',
-                  'Art: A world History',
-                  'http://ecx.images-amazon.com/images/I/514dfpLu5GL._SL160_AA115_.jpg',
-                  3,
-                  95,
-                  [ 'manual'],
-                  false),
-  new sample.Book('84823221177',
-                  "The Sagan Diary",
-                  'http://ecx.images-amazon.com/images/I/51FlsY9OKVL._SL160_AA115_.jpg',
-                  5,
-                  10.3,
-                  [ 'sci-fi', 'crime'],
-                  true),
-  new sample.Book('14823221177',
-                  "The Last Patrion",
-                  'http://ecx.images-amazon.com/images/I/51de0isP05L._SL500_SL150_.jpg',
-                  1,
-                  43,
-                  [ 'politics', 'crime'],
-                  true),
-  new sample.Book('84823421177',
-                  "Brisingr (Inheritance)",
-                  'http://ecx.images-amazon.com/images/I/51ynF4PIThL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
-                  2,
-                  72,
-                  [ 'fantasy', 'romance'],
-                  false),
-  new sample.Book('84823226677',
-                  "Rediscover God in America",
-                  'http://ecx.images-amazon.com/images/I/41VR8ZKNKKL._SL500_SL150_.jpg',
-                  4,
-                  35,
-                  [ 'politics'],
-                  true),
-  new sample.Book('84823221187',
-                  "The Digital Photography Book",
-                  'http://ecx.images-amazon.com/images/I/4127fI6pghL._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
-                  4,
-                  24.3,
-                  [ 'manual'],
-                  true),
-  new sample.Book('848000000',
-                  "Apple Aperture 2",
-                  'http://ecx.images-amazon.com/images/I/51PmGTgxJ6L._SL160_PIsitb-sticker-arrow-dp,TopRight,12,-18_SH30_OU01_AA115_.jpg',
-                  3,
-                  43,
-                  [ 'programming', 'manual'],
-                  false),
-  new sample.Book('848232890077',
-                  "Anne Frank: The Diary",
-                  'http://ecx.images-amazon.com/images/I/519HKX9M69L._SL500_SL150_.jpg',
-                  5,
-                  10.3,
-                  [ 'history'],
-                  true)
-];
-
-var metamodel = {
-  isbn: { kind: rhizo.meta.Kind.STRING, label: "ISBN" },
-  title: { kind: rhizo.meta.Kind.STRING, label: "Title" },  
-  rating: { kind: rhizo.meta.Kind.RANGE, label: "Rating", min: 1, max: 5, stepping: 1},
-  price: { kind: rhizo.meta.Kind.RANGE, label: "Price", min: 1, max: 100},
-  paperback: { kind: rhizo.meta.Kind.BOOLEAN, label: "Paperback" },
-  genres: { kind: rhizo.meta.Kind.CATEGORY, label: "Genres" , 
-            categories: [ 'sci-fi', 'fantasy', 'crime' , 'politics', 'fiction', 'romance', 'programming', 'manual', 'history'] , 
-            multiple: true}
-};
-
-var renderer = {
-  render: function(model) {
-    return $("<div style='padding: 5px'>" + 
-             "<p style='white-space: nowrap; overflow:hidden'><img src='" + model.img + "' align='left' width='75px'>" + 
-             "<b>" + model.title + "</b><br />" +
-             "Rating: " + this.stars(model) + "<br />" +
-             "Price: " + this.price(model) + "</p>" +
-             "</div>");
-  },
-  stars: function(model) {
-    var str = [];
-    for (i = 0; i < model.rating; i++) {
-      str.push("<img src='static/samples/img/books/star.png'>");
+  var renderer = {
+    render: function(model) {
+      return $("<div style='padding: 5px'>" + 
+               "<p style='white-space: nowrap; overflow:hidden'>" +
+               "<img src='" + model.img + "' align='left' width='75px'>" + 
+               "<b>" + model.title + "</b><br />" +
+               "Rating: " + this.stars(model) + "<br />" +
+               "Price: " + this.price(model) + "</p>" +
+               "</div>");
+    },
+    stars: function(model) {
+      var str = [];
+      for (i = 0; i < model.rating; i++) {
+        str.push("<img src='static/samples/img/books/star.png'>");
+      }
+      return str.join("");
+    },
+    price: function(model) {
+      if (model.price > 50) {
+        return "<span style='color: red'>" + model.price + " $</span>";
+      } else {
+        return "" + model.price + " $";
+      }
     }
-    return str.join("");
-  },
-  price: function(model) {
-    if (model.price > 50) {
-      return "<span style='color: red'>" + model.price + " $</span>";
-    } else {
-      return "" + model.price + " $";
-    }
-  }
-};
+  };
 
-// hack to remove the tree layout in this demo
-// TODO(battlehorse): This disrupts multisphere isolation.
-delete rhizo.layout.layouts.tree;
+  // hack to remove the tree layout in this demo
+  // TODO(battlehorse): This disrupts multisphere isolation.
+  delete rhizo.layout.layouts.tree;
 
-{{ jsonp_callback }}({
-    'renderer': renderer,
-    'metamodel': metamodel,
-    'models': models
-});
+  {{ jsonp_callback }}({
+      'renderer': renderer,
+      'metamodel': metamodel,
+      'models': models
+  });
+})();
--- samples/js/picasa.js
+++ samples/js/picasa.js
@@ -14,116 +14,116 @@
   limitations under the License.
 */
 
-namespace("picasa");
+(function() {
+  // Load an additional library we need to create photo-frame effects to 
+  // thumbnails
+  var e = document.createElement("script");
+  e.src = "sample/cvi_instant_lib.js";
+  e.type="text/javascript";
+  document.getElementsByTagName("head")[0].appendChild(e);
 
-// Load an additional library we need to create photo-frame effects to 
-// thumbnails
-var e = document.createElement("script");
-e.src = "sample/cvi_instant_lib.js";
-e.type="text/javascript";
-document.getElementsByTagName("head")[0].appendChild(e);
-
-// Model definition
-var models = [];
-var tilts = [ 'l', 'n', 'r'];
-picasa.Photo = function(item) {
-  this.id = item.gphoto$id['$t'];
-  this.author = item.author[0].name['$t'];
-  this.title = item.title['$t'];
-  this.summary =  item.summary['$t'];
-  this.albumtitle = item.gphoto$albumtitle['$t'];
-  this.thumbnail = item.media$group.media$thumbnail[0];
-  this.mpixel = item.media$group.media$content[0].width * 
-                item.media$group.media$content[0].height / (1024.0 * 1024.0);
-  this.bigthumbnail = item.media$group.media$content[0];  
-  this.tilt = tilts[Math.floor(Math.random()*tilts.length)];
+  // Model definition
+  var models = [];
+  var tilts = [ 'l', 'n', 'r'];
+  var Photo = function(item) {
+    this.id = item.gphoto$id['$t'];
+    this.author = item.author[0].name['$t'];
+    this.title = item.title['$t'];
+    this.summary =  item.summary['$t'];
+    this.albumtitle = item.gphoto$albumtitle['$t'];
+    this.thumbnail = item.media$group.media$thumbnail[0];
+    this.mpixel = item.media$group.media$content[0].width * 
+      item.media$group.media$content[0].height / (1024.0 * 1024.0);
+    this.bigthumbnail = item.media$group.media$content[0];  
+    this.tilt = tilts[Math.floor(Math.random()*tilts.length)];
   
-  var regex = new RegExp("^(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)");
-  var results = regex.exec(item.published['$t']);
-  this.published = new Date(
+    var regex = new RegExp("^(\\d\\d\\d\\d)-(\\d\\d)-(\\d\\d)");
+    var results = regex.exec(item.published['$t']);
+    this.published = new Date(
       parseInt(results[1], 10),
       parseInt(results[2], 10)-1,
       parseInt(results[3], 10));
-};
+  };
 
-// Renderer definition
-var renderer = {
-  render: function(model, expanded, opt_options) {
-    if (!expanded) {
-      var container = $("<div style='padding: 5px'></div>");
-      var img = $("<img src='" + model.thumbnail.url + 
-                  "' width='" + model.thumbnail.width + 
-                  "' height='" + model.thumbnail.height + 
-                  "' />").appendTo(container);      
-      cvi_instant.add(img.get(0), {tilt: model.tilt});
-      $("<p style='font-size:10px'>by <b>" +  
-        model.author + 
-        '</b></p>').appendTo(container);
-      return container;
-    } else {
-      return $("<div style='padding: 5px'>" +
-               "<p style='font-size:10px'>" +
-               'by <b>' +  model.author + '</b></p>' +
-               '<p>' + model.summary + '</p>' +
-               "</div>");
-    }
-  },
-  expandable: true
-};
-
-// Choose a tag (either a default or extracted from the URL) to query picasa with
-var tag = 'flower';
-var tagregex = new RegExp('tag=([^&]*)');
-var tagresults = tagregex.exec(document.location.href);
-if (tagresults && tagresults[1]) {
-  tag = unescape(tagresults[1]);
-}
-
-// Number of models to show
-var limit = 200;
-var url = 'http://picasaweb.google.com/data/feed/base/all?alt=json&kind=photo' +
-          '&access=public&tag=' + tag + '&filter=1&hl=en_US&callback=?';
-          
-// Fetch content from Picasa
-$.getJSON(
-  url,
-  function(data) {
-    $.each(data.feed.entry, function(i, item) {
-      if (i < limit) {
-        models.push(new picasa.Photo(item));
+  // Renderer definition
+  var renderer = {
+    render: function(model, expanded, opt_options) {
+      if (!expanded) {
+        var container = $("<div style='padding: 5px'></div>");
+        var img = $("<img src='" + model.thumbnail.url + 
+                    "' width='" + model.thumbnail.width + 
+                    "' height='" + model.thumbnail.height + 
+                    "' />").appendTo(container);      
+        cvi_instant.add(img.get(0), {tilt: model.tilt});
+        $("<p style='font-size:10px'>by <b>" +  
+          model.author + 
+          '</b></p>').appendTo(container);
+        return container;
+      } else {
+        return $("<div style='padding: 5px'>" +
+                 "<p style='font-size:10px'>" +
+                 'by <b>' +  model.author + '</b></p>' +
+                 '<p>' + model.summary + '</p>' +
+                 "</div>");
       }
+    },
+    expandable: true
+  };
+
+  // Choose a tag (either a default or extracted from the URL) to query picasa with
+  var tag = 'flower';
+  var tagregex = new RegExp('tag=([^&]*)');
+  var tagresults = tagregex.exec(document.location.href);
+  if (tagresults && tagresults[1]) {
+    tag = unescape(tagresults[1]);
+  }
+
+  // Number of models to show
+  var limit = 200;
+  var url = 'http://picasaweb.google.com/data/feed/base/all?alt=json&kind=photo' +
+     '&access=public&tag=' + tag + '&filter=1&hl=en_US&callback=?';
+          
+  // Fetch content from Picasa
+  $.getJSON(
+    url,
+    function(data) {
+      $.each(data.feed.entry, function(i, item) {
+        if (i < limit) {
+          models.push(new Photo(item));
+        }
+      });
+    
+      // Post-processing to identify model characteristics and ranges required
+      // by the metaModel.
+      var minResolution = 100;
+      var maxResolution = 0;
+      var minYear = 3000;
+      var maxYear = 0;
+      $.each(models, function(i, model) {
+        minResolution = Math.min(minResolution, model.mpixel);
+        maxResolution = Math.max(maxResolution, model.mpixel);  
+        minYear = Math.min(minYear, model.published.getFullYear());
+        maxYear = Math.max(maxYear, model.published.getFullYear());          
+      });
+    
+      // Build the metamodel
+      var decimalKind = new rhizo.meta.DecimalRangeKind(2);
+      decimalKind.toHumanLabel_ = rhizo.ui.toHumanLabel;
+    
+      var metamodel = {
+        author: { kind: rhizo.meta.Kind.STRING, label: "Name" },
+        title: { kind: rhizo.meta.Kind.STRING, label: "Title" },
+        mpixel: { kind: decimalKind, label: "Resolution (MP)", 
+                  min: minResolution, max: maxResolution },
+        published: { kind: new rhizo.meta.DateKind('y'), label: "Published", 
+                     minYear: minYear, maxYear: maxYear }
+      };
+    
+      // Roll everything out.
+      {{ jsonp_callback }}({
+          'renderer': renderer,
+          'metamodel': metamodel,
+          'models': models
+      });
     });
-    
-    // Post-processing to identify model characteristics and ranges required
-    // by the metaModel.
-    var minResolution = 100;
-    var maxResolution = 0;
-    var minYear = 3000;
-    var maxYear = 0;
-    $.each(models, function(i, model) {
-      minResolution = Math.min(minResolution, model.mpixel);
-      maxResolution = Math.max(maxResolution, model.mpixel);  
-      minYear = Math.min(minYear, model.published.getFullYear());
-      maxYear = Math.max(maxYear, model.published.getFullYear());          
-    });
-    
-    // Build the metamodel
-    var decimalKind = new rhizo.meta.DecimalRangeKind(2);
-    decimalKind.toHumanLabel_ = rhizo.ui.toHumanLabel;
-    
-    var metamodel = {
-      author: { kind: rhizo.meta.Kind.STRING, label: "Name" },
-      title: { kind: rhizo.meta.Kind.STRING, label: "Title" },
-      mpixel: { kind: decimalKind, label: "Resolution (MP)", 
-                min: minResolution, max: maxResolution },
-      published: { kind: new rhizo.meta.DateKind('y'), label: "Published", 
-                   minYear: minYear, maxYear: maxYear }
-    };
-    
-    // Roll everything out.
-    {{ jsonp_callback }}({
-        'renderer': renderer,
-        'metamodel': metamodel,
-        'models': models
-    });
-  });
+})();
