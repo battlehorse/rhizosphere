b5106cb3be7eaf3c4f93ad40373c1c4a5b1c6335
Riccardo Govoni <battlehorse@gmail.com>
2010-08-06 01:56:53 +0100

    Fixed copyright date

--- samples/js/factbook.js
+++ samples/js/factbook.js
@@ -1,5 +1,5 @@
 /*
-  Copyright 2008 Riccardo Govoni battlehorse@gmail.com
+  Copyright 2010 Riccardo Govoni battlehorse@gmail.com
 
   Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
   you may not use this file except in compliance with the License.
