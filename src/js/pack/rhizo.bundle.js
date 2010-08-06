6ee81914f516dc6d8bbcda7641d18146d27fd70d
Riccardo Govoni <battlehorse@google.com>
2010-08-06 15:27:26 +0100

    Updated packer file

--- src/js/pack/rhizo.bundle.js
+++ src/js/pack/rhizo.bundle.js
@@ -200,10 +200,19 @@ rhizo.ui.defaultRenderingRescaler = function() {};
 
 rhizo.ui.defaultRenderingRescaler.prototype.rescale = function(naked_render,
                                                                width,
-                                                               height) {
+                                                               height,
+                                                               opt_failure_callback) {
   // TODO(battlehorse): should rescaling be animated?
-  naked_render.width(width - naked_render.outerWidth(true) + naked_render.width());
-  naked_render.height(height - naked_render.outerHeight(true) + naked_render.height());
+  width = width - naked_render.outerWidth(true) + naked_render.width();
+  height = height - naked_render.outerHeight(true) + naked_render.height();
+  if (width > 0 && height > 0) {
+    naked_render.width(width).height(height);
+    return true;
+  }
+  if (opt_failure_callback) {
+    opt_failure_callback();
+  }
+  return false;
 };
 /* ./src/js/rhizo.meta.js */
 /*
@@ -931,6 +940,172 @@ rhizo.meta.Kind.DECIMAL = new rhizo.meta.DecimalKind();
 rhizo.meta.Kind.DECIMALRANGE = new rhizo.meta.DecimalRangeKind();
 rhizo.meta.Kind.LOGARITHMRANGE = new rhizo.meta.LogarithmRangeKind();
 rhizo.meta.Kind.STRINGARRAY = new rhizo.meta.StringArrayKind();
+/* ./src/js/rhizo.model.js */
+/*
+  Copyright 2008 Riccardo Govoni battlehorse@gmail.com
+
+  Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+// RHIZODEP=rhizo.ui
+namespace("rhizo.model");
+
+rhizo.model.SuperModel = function(model, opt_selected, opt_filtered) {
+  this.model = model;
+  this.id = model.id;
+  this.selected = opt_selected || false;
+  this.filters_ = {}; // a map of filter status, one for each model key
+  this.rendering = null;
+  this.expanded = false; // whether the rendering is expanded or not
+
+  this.renderingRescaler = null;
+};
+
+rhizo.model.SuperModel.prototype.unwrap = function() {
+  return this.model;
+};
+
+rhizo.model.SuperModel.prototype.toString = function() {
+  return this.model.toString();
+};
+
+rhizo.model.SuperModel.prototype.isFiltered = function(opt_key) {
+  if (opt_key) {
+    return this.filters_[opt_key] || false;
+  } else {
+    var countFilters = 0;
+    for (key in this.filters_) { countFilters++;}
+    return countFilters != 0;
+  }
+};
+
+rhizo.model.SuperModel.prototype.filter = function(key) {
+  this.filters_[key] = true;
+};
+
+rhizo.model.SuperModel.prototype.resetFilter = function(key) {
+  delete this.filters_[key];
+};
+
+rhizo.model.SuperModel.prototype.rescaleRendering = function(
+    width, height, opt_failure_callback) {
+  return this.renderingRescaler.rescale(
+    $('.rhizo-naked-render', this.rendering),
+    width, height, opt_failure_callback);
+};
+
+rhizo.model.kickstart = function(model, project, renderer, opt_options) {
+  var rendering = $('<div class="rhizo-model"></div>');
+  rhizo.ui.reRender(
+      renderer, rendering,
+      model.unwrap(), // rendered expects the naked model
+      model.expanded, // initial expansion status, dictated by the SuperModel
+      opt_options);
+
+  // Add the maximize icon, if the renderer supports expansion
+  if (rhizo.ui.expandable(renderer, opt_options)) {
+    var expander = $('<div class="rhizo-expand-model" ' +
+                     'style="display:none"></div>');
+    expander.data("id", model.id);
+    rendering.append(expander);
+  }
+
+  // enrich the super model
+  model.rendering = rendering;
+  if (typeof(renderer.rescale) == 'function') {
+    model.renderingRescaler = renderer;
+  } else {
+    model.renderingRescaler = new rhizo.ui.defaultRenderingRescaler();
+  }
+
+  rendering.data("id", model.id);
+  rendering.dblclick(function() {
+    if (project.isSelected($(this).data("id"))) {
+      project.unselect($(this).data("id"));
+    } else {
+      project.select($(this).data("id"));
+    }
+  });
+
+  rendering.draggable({
+    opacity: 0.7,
+    cursor: 'pointer',
+    zIndex: 10000,
+    distance: 3,
+    start: function(ev, ui) {
+      project.toggleSelection('disable');
+      // used by droppable feature
+      ui.helper.data(
+          "dropTop0",
+          parseInt(ui.helper.css("top"),10));
+      ui.helper.data(
+          "dropLeft0",
+          parseInt(ui.helper.css("left"),10));
+
+      // figure out all the initial positions for the selected elements
+      // and store them.
+      if (project.isSelected(ui.helper.data("id"))) {
+        var all_selected = project.allSelected();
+        for (var id in all_selected) {
+          var selected_rendering = all_selected[id].rendering;
+          selected_rendering.data(
+            "top0",
+            parseInt(selected_rendering.css("top"),10) -
+              parseInt(ui.helper.css("top"),10));
+          selected_rendering.data(
+            "left0",
+            parseInt(selected_rendering.css("left"),10) -
+              parseInt(ui.helper.css("left"),10));
+
+          // used by droppable feature
+          selected_rendering.data("dropTop0",
+                                  parseInt(selected_rendering.css("top"),10));
+          selected_rendering.data("dropLeft0",
+                                  parseInt(selected_rendering.css("left"),10));
+        }
+      }
+    },
+    drag: function(ev, ui) {
+      var drag_helper_id = ui.helper.data("id");
+      if (project.isSelected(drag_helper_id)) {
+        var all_selected = project.allSelected();
+        for (var id in all_selected) {
+          if (id != drag_helper_id) {
+            all_selected[id].rendering.css(
+                'top',
+                all_selected[id].rendering.data("top0") + ui.position.top);
+            all_selected[id].rendering.css(
+                'left',
+                all_selected[id].rendering.data("left0") + ui.position.left);
+          }
+        }
+      }
+    },
+    stop: function(ev, ui) {
+      project.toggleSelection('enable');
+      if (project.isSelected(ui.helper.data("id"))) {
+        var all_selected = project.allSelected();
+        for (var id in all_selected) {
+          all_selected[id].rendering.removeData("top0");
+          all_selected[id].rendering.removeData("left0");
+        }
+      }
+    },
+    refreshPositions: false
+  });
+
+  return rendering;
+};
 /* ./src/js/rhizo.layout.js */
 /*
   Copyright 2008 Riccardo Govoni battlehorse@gmail.com
@@ -1218,171 +1393,6 @@ rhizo.layout.layouts = {
   flow: rhizo.layout.FlowLayout,
   scramble: rhizo.layout.ScrambleLayout,
   bucket: rhizo.layout.BucketLayout
-};
-/* ./src/js/rhizo.model.js */
-/*
-  Copyright 2008 Riccardo Govoni battlehorse@gmail.com
-
-  Licensed under the Apache License, Version 2.0 (the &quot;License&quot;);
-  you may not use this file except in compliance with the License.
-  You may obtain a copy of the License at
-
-      http://www.apache.org/licenses/LICENSE-2.0
-
-  Unless required by applicable law or agreed to in writing, software
-  distributed under the License is distributed on an &quot;AS IS&quot; BASIS,
-  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-  See the License for the specific language governing permissions and
-  limitations under the License.
-*/
-
-// RHIZODEP=rhizo.ui
-namespace("rhizo.model");
-
-rhizo.model.SuperModel = function(model, opt_selected, opt_filtered) {
-  this.model = model;
-  this.id = model.id;
-  this.selected = opt_selected || false;
-  this.filters_ = {}; // a map of filter status, one for each model key
-  this.rendering = null;
-  this.expanded = false; // whether the rendering is expanded or not
-
-  this.renderingRescaler = null;
-};
-
-rhizo.model.SuperModel.prototype.unwrap = function() {
-  return this.model;
-};
-
-rhizo.model.SuperModel.prototype.toString = function() {
-  return this.model.toString();
-};
-
-rhizo.model.SuperModel.prototype.isFiltered = function(opt_key) {
-  if (opt_key) {
-    return this.filters_[opt_key] || false;
-  } else {
-    var countFilters = 0;
-    for (key in this.filters_) { countFilters++;}
-    return countFilters != 0;
-  }
-};
-
-rhizo.model.SuperModel.prototype.filter = function(key) {
-  this.filters_[key] = true;
-};
-
-rhizo.model.SuperModel.prototype.resetFilter = function(key) {
-  delete this.filters_[key];
-};
-
-rhizo.model.SuperModel.prototype.rescaleRendering = function(width, height) {
-  this.renderingRescaler.rescale(
-    $('.rhizo-naked-render', this.rendering),
-    width, height);
-};
-
-rhizo.model.kickstart = function(model, project, renderer, opt_options) {
-  var rendering = $('<div class="rhizo-model"></div>');
-  rhizo.ui.reRender(
-      renderer, rendering,
-      model.unwrap(), // rendered expects the naked model
-      model.expanded, // initial expansion status, dictated by the SuperModel
-      opt_options);
-
-  // Add the maximize icon, if the renderer supports expansion
-  if (rhizo.ui.expandable(renderer, opt_options)) {
-    var expander = $('<div class="rhizo-expand-model" ' +
-                     'style="display:none"></div>');
-    expander.data("id", model.id);
-    rendering.append(expander);
-  }
-
-  // enrich the super model
-  model.rendering = rendering;
-  if (typeof(renderer.rescale) == 'function') {
-    model.renderingRescaler = renderer;
-  } else {
-    model.renderingRescaler = new rhizo.ui.defaultRenderingRescaler();
-  }
-
-  rendering.data("id", model.id);
-  rendering.dblclick(function() {
-    if (project.isSelected($(this).data("id"))) {
-      project.unselect($(this).data("id"));
-    } else {
-      project.select($(this).data("id"));
-    }
-  });
-
-  rendering.draggable({
-    opacity: 0.7,
-    cursor: 'pointer',
-    zIndex: 10000,
-    distance: 3,
-    start: function(ev, ui) {
-      project.toggleSelection('disable');
-      // used by droppable feature
-      ui.helper.data(
-          "dropTop0",
-          parseInt(ui.helper.css("top"),10));
-      ui.helper.data(
-          "dropLeft0",
-          parseInt(ui.helper.css("left"),10));
-
-      // figure out all the initial positions for the selected elements
-      // and store them.
-      if (project.isSelected(ui.helper.data("id"))) {
-        var all_selected = project.allSelected();
-        for (var id in all_selected) {
-          var selected_rendering = all_selected[id].rendering;
-          selected_rendering.data(
-            "top0",
-            parseInt(selected_rendering.css("top"),10) -
-              parseInt(ui.helper.css("top"),10));
-          selected_rendering.data(
-            "left0",
-            parseInt(selected_rendering.css("left"),10) -
-              parseInt(ui.helper.css("left"),10));
-
-          // used by droppable feature
-          selected_rendering.data("dropTop0",
-                                  parseInt(selected_rendering.css("top"),10));
-          selected_rendering.data("dropLeft0",
-                                  parseInt(selected_rendering.css("left"),10));
-        }
-      }
-    },
-    drag: function(ev, ui) {
-      var drag_helper_id = ui.helper.data("id");
-      if (project.isSelected(drag_helper_id)) {
-        var all_selected = project.allSelected();
-        for (var id in all_selected) {
-          if (id != drag_helper_id) {
-            all_selected[id].rendering.css(
-                'top',
-                all_selected[id].rendering.data("top0") + ui.position.top);
-            all_selected[id].rendering.css(
-                'left',
-                all_selected[id].rendering.data("left0") + ui.position.left);
-          }
-        }
-      }
-    },
-    stop: function(ev, ui) {
-      project.toggleSelection('enable');
-      if (project.isSelected(ui.helper.data("id"))) {
-        var all_selected = project.allSelected();
-        for (var id in all_selected) {
-          all_selected[id].rendering.removeData("top0");
-          all_selected[id].rendering.removeData("left0");
-        }
-      }
-    },
-    refreshPositions: false
-  });
-
-  return rendering;
 };
 /* ./src/js/rhizo.autorender.js */
 /*
@@ -3757,54 +3767,67 @@ rhizo.layout.TreeMapLayout.prototype.layout = function(container,
     currentSlice.addNode(node);
 
   }
-  if (numModelsToHide > 0) {
-    this.project_.alignVisibility();
-  }
   if (slices.length > 0) {
     this.managedModels_ = {};
-    this.draw_(container, slices, colorRange);
+    numModelsToHide += this.draw_(container, slices, colorRange);
+  }
+  if (numModelsToHide > 0) {
+    this.project_.alignVisibility();
   }
 };
 
 rhizo.layout.TreeMapLayout.prototype.draw_ = function(container, slices, colorRange) {
+  var numModelsToHide = 0;
   for (var i = 0; i < slices.length; i++) {
-    this.drawSlice_(container, slices[i], colorRange);
+    numModelsToHide += this.drawSlice_(container, slices[i], colorRange);
   }
+  return numModelsToHide;
 };
 
 rhizo.layout.TreeMapLayout.prototype.drawSlice_ = function(container, slice, colorRange) {
+  var numModelsToHide = 0;
   var t = slice.anchorPoint().y;
   var l = slice.anchorPoint().x;
   for (var i = 0; i < slice.nodes().length; i++) {
     var length = slice.nodes()[i].area() / slice.span();
     var model = slice.nodes()[i].model();
-    this.managedModels_[model.id] = model;
-
-    if (colorRange) {
-      var colorVal = parseFloat(model.unwrap()[colorRange.meta]);
-      if (!isNaN(colorVal)) {
-        model.rendering.css('backgroundColor',
-                            this.getBackgroundColor_(colorVal,
-                                                     colorRange));
+    
+    if (Math.round(length) == 0 || Math.round(slice.span()) == 0) {
+      // Hide items that are too small to be displayed on screen
+      model.filter('__treemap__');
+      numModelsToHide++;
+    } else {
+      var renderingSize = {};
+      if (slice.direction() == rhizo.layout.treemap.TreeMapDirection.HOR) {
+        renderingSize['width'] = Math.round(length);
+        renderingSize['height'] = Math.round(slice.span());
+      } else {
+        renderingSize['width'] = Math.round(slice.span());
+        renderingSize['height'] = Math.round(length);
+      }
+      if (!model.rescaleRendering(renderingSize['width'], renderingSize['height'])) {
+        model.filter('__treemap__');
+        numModelsToHide++;
+      } else {
+        this.managedModels_[model.id] = model;
+        if (colorRange) {
+          var colorVal = parseFloat(model.unwrap()[colorRange.meta]);
+          if (!isNaN(colorVal)) {
+            model.rendering.css('backgroundColor',
+                                this.getBackgroundColor_(colorVal,
+                                                         colorRange));
+          }
+        }
+        model.rendering.move(Math.round(t), Math.round(l));        
       }
     }
-
-    var renderingSize = {};
-    if (slice.direction() == rhizo.layout.treemap.TreeMapDirection.HOR) {
-      renderingSize['width'] = Math.round(length);
-      renderingSize['height'] = Math.round(slice.span());
-    } else {
-      renderingSize['width'] = Math.round(slice.span());
-      renderingSize['height'] = Math.round(length);
-    }
-    model.rescaleRendering(renderingSize['width'], renderingSize['height']);
-    model.rendering.move(Math.round(t), Math.round(l));
     if (slice.direction() == rhizo.layout.treemap.TreeMapDirection.HOR) {
       l += length;
     } else {
       t += length;
     }
   }
+  return numModelsToHide;
 };
 
 rhizo.layout.TreeMapLayout.prototype.getBackgroundColor_ = function(colorVal,
