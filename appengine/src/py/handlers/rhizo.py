6fe72711f0164953bdd4c5e743f4fa61f2383c01
Riccardo Govoni <battlehorse@google.com>
2010-09-16 15:50:14 +0100

    Renamed startup url parameters (simulate={mobile,small}) to match the parameter names used by the js code (forcePlatform, forceDevice, forceTemplate). Added serverside iPad detection via useragent.

--- appengine/src/py/handlers/rhizo.py
+++ appengine/src/py/handlers/rhizo.py
@@ -11,14 +11,37 @@ from py import rhizoglobals
 
 class RhizoHandler(webapp.RequestHandler):
 
+    def _getOptionFromUrl(self, optname, allowedvalues):
+      opt = self.request.get(optname)
+      if opt and opt not in allowedvalues:
+        return allowedvalues[0]
+      return opt
+
+    def _getPlatformDevice(self):
+      platform, device = None, None
+      user_agent = self.request.headers.get('User-Agent', '').lower()
+      if 'ipad' in user_agent:
+          platform, device = 'mobile', 'ipad'
+      if not platform:
+        platform = self._getOptionFromUrl('forcePlatform',
+                                          ['default', 'mobile'])
+      if not device:
+        device = self._getOptionFromUrl('forceDevice',
+                                        ['default', 'ipad'])
+      return platform, device
+
     def get(self):
+        # Should we serve compiled or uncompiled resources?
         debug = self.request.get('d', '0') == '1'
-        simulatesmall = self.request.get('simulate', '') == 'small'
-        simulatemobile = self.request.get('simulate', '') == 'mobile'
+
+        # Startup options
+        forcePlatform, forceDevice = self._getPlatformDevice()
+        forceTemplate = self._getOptionFromUrl('forceTemplate',
+                                               ['default', 'bottom'])
         template_values = {'debug': debug,
-                           'small': simulatesmall,
-                           'mobile': simulatemobile,
-                          }
+                           'forceTemplate': forceTemplate,
+                           'forceDevice': forceDevice,
+                           'forcePlatform': forcePlatform,}
         path = os.path.join(os.path.dirname(__file__),
                             '../../templates%s' % self.request.path)
         self.response.out.write(template.render(path, template_values))
