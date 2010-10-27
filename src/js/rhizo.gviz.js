e2a237f9175e6e33dd5bd0d6f2082cdae6e309b4
dinoderek <devnull@localhost>
2010-10-27 17:53:02 +0100

    GViz friendly interface

---
+++ src/js/rhizo.gviz.js
@@ -0,0 +1,251 @@
+/*
+  Copyright 2008 The Rhizosphere Authors. All Rights Reserved.
+
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+
+      http://www.apache.org/licenses/LICENSE-2.0
+
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+// RHIZODEP=rhizo.meta,rhizo.autorender
+namespace("rhizo.gviz");
+
+/**
+ * Wrapper to expose Rhizosphere as a Google Visualization.
+ *
+ * @param {HTMLElement} container The element that will contain the
+ *    visualization.
+ * @constructor
+ */
+rhizo.gviz.Rhizosphere = function(container) {
+  this.container_ = container;
+};
+
+/**
+ *
+ * @param {google.visualization.DataTable} datatable
+ * @param {*} opt_options
+ */
+rhizo.gviz.Rhizosphere.prototype.draw = function(datatable, opt_options) {
+  var bootstrapper = new rhizo.bootstrap.Bootstrap(this.container_, opt_options);
+
+  // TODO(battlehorse): replace with proper logger.
+  var logger = {warn: function() {}};
+  var initializer = new rhizo.gviz.Initializer(datatable, logger, opt_options);
+  bootstrapper.prepare();
+  bootstrapper.deployExplicit(initializer.metamodel,
+                              initializer.renderer,
+                              initializer.models);
+};
+
+rhizo.gviz.Initializer = function(dataTable,
+                                  logger,
+                                  opt_options,
+                                  opt_customRenderer) {
+  this.dt_ = dataTable;
+  this.logger_ = logger;
+  this.options_ = opt_options || {};
+  this.customRenderer_ = opt_customRenderer;
+
+  this.init_();
+};
+
+rhizo.gviz.Initializer.prototype.init_ = function() {
+  this.metamodel = this.buildMetaModel_();
+  this.models = this.loadModels_(this.metamodel);
+  this.renderer = this.customRenderer_ ?
+                  this.customRenderer_ :
+                  this.createDefaultRenderer_(this.metamodel, this.models);
+};
+
+rhizo.gviz.Initializer.prototype.buildMetaModel_ = function() {
+  var metamodel = {};
+  for (var i = 0, len = this.dt_.getNumberOfColumns(); i < len; i++) {
+    var id = this.dt_.getColumnId(i);
+    metamodel[id] = {};
+
+    // parsing label
+    metamodel[id].label = this.dt_.getColumnLabel(i);
+    if (metamodel[id].label == '') {
+      metamodel[id].label = "Column " + id;
+    }
+
+    // parsing kind
+    var type = this.dt_.getColumnType(i);
+    if (type == 'number') {
+      var min = this.dt_.getColumnRange(i).min;
+      var max = this.dt_.getColumnRange(i).max;
+      if (min == max) {
+        metamodel[id].kind = rhizo.meta.Kind.NUMBER;
+      } else {
+        metamodel[id].kind = rhizo.meta.Kind.RANGE;
+        metamodel[id].min = min;
+        metamodel[id].max = max;
+      }
+    } else if (type == 'boolean') {
+      metamodel[id].kind = rhizo.meta.Kind.BOOLEAN;
+    } else {
+      // assumed string type
+      if (type != 'string') {
+        this.logger_.warn(
+            "Column " + metamodel[id].label +
+            " will be treated as String. Unsupported type: " + type);
+      }
+
+      if (metamodel[id].label.indexOf("CAT") != -1) {
+        // if column title contains the word CAT, assume it's a category
+        // yeah, I know, pretty crappy way of identifying categories.
+        metamodel[id].kind = rhizo.meta.Kind.CATEGORY;
+        metamodel[id].label =
+            metamodel[id].label.replace("CAT","").replace(/^\s+|\s+$/g, "");
+        metamodel[id].categories = this.parseCategories_(i);
+
+        if (metamodel[id].label.indexOf("MUL") != -1) {
+          metamodel[id].label =
+              metamodel[id].label.replace("MUL","").replace(/^\s+|\s+$/g, "");
+          metamodel[id].multiple = true;
+        }
+      } else {
+        metamodel[id].kind = rhizo.meta.Kind.STRING;
+      }
+    }
+
+    // parsing autorender attributes, if any
+    this.buildAutoRenderInfo_(metamodel[id], id);
+  }
+  return metamodel;
+};
+
+
+rhizo.gviz.Initializer.prototype.parseSingleCategory_ = function(value) {
+  var categoriesMap = {};
+  var rawCats = value.split(',');
+  var prunedCats = $.grep(rawCats, function(category) {
+    return category != '';
+  });
+
+  for (var i = 0; i < prunedCats.length; i++) {
+    categoriesMap[prunedCats[i].replace(/^\s+|\s+$/g, "")] = true;
+  }
+
+  var categories = [];
+  for (category in categoriesMap) {
+    categories.push(category);
+  }
+
+  return categories;
+};
+
+rhizo.gviz.Initializer.prototype.parseCategories_ = function(columnIndex) {
+  var categoriesMap = {};
+  for (var i = 0, len = this.dt_.getNumberOfRows(); i < len; i++) {
+    var rowCategories = this.parseSingleCategory_(
+        this.dt_.getValue(i, columnIndex));
+    for (var r = 0; r < rowCategories.length; r++) {
+      categoriesMap[rowCategories[r]] = true;
+    }
+  }
+
+  var categories = [];
+  for (category in categoriesMap) {
+    categories.push(category);
+  }
+
+  return categories.sort();
+};
+
+rhizo.gviz.Initializer.prototype.buildAutoRenderInfo_ =
+    function(metamodelEntry, id) {
+  var ar = {};
+  var hasArAttribute = false;
+  if (this.options_.arMaster &&
+      this.matchAutoRenderOption_(
+          this.options_.arMaster, metamodelEntry.label, id)) {
+    ar.master = true;
+    hasArAttribute = true;
+  }
+  if (this.options_.arSize &&
+      this.matchAutoRenderOption_(
+          this.options_.arSize, metamodelEntry.label, id)) {
+    ar.bind = (ar.bind ? ar.bind : '') + 'size ';
+    hasArAttribute = true;
+  }
+  if (this.options_.arColor &&
+      this.matchAutoRenderOption_(
+          this.options_.arColor, metamodelEntry.label, id)) {
+    ar.bind = (ar.bind ? ar.bind : '') + 'color ';
+    hasArAttribute = true;
+  }
+  if (hasArAttribute) {
+    metamodelEntry.ar = ar;
+  }
+};
+
+rhizo.gviz.Initializer.prototype.matchAutoRenderOption_ =
+    function(optionValue, label, id) {
+  var colRegExp = /^[a-zA-Z]$/;
+  if (colRegExp.test(optionValue)) {
+    // try matching the column header
+    return optionValue.toLowerCase() == new String(id).toLowerCase();
+
+  } else {
+
+    // otherwise try to match the column label
+    if (label.toLowerCase() == optionValue.toLowerCase()) {
+      return true;
+    } else {
+
+      // if still unsuccessful, verify that category tags
+      // are not causing the problem
+      return label.replace("CAT", "").replace("MUL", "").toLowerCase() ==
+             optionValue.toLowerCase();
+    }
+  }
+};
+
+rhizo.gviz.Initializer.prototype.loadModels_ = function(metamodel) {
+  var models = [];
+  for (var i = 0, len = this.dt_.getNumberOfRows(); i < len; i++) {
+    var model = {};
+    for (var j = 0, clen = this.dt_.getNumberOfColumns(); j < clen; j++) {
+      model.id = "gviz-" + i;
+      var value = this.dt_.getValue(i, j);
+      if (metamodel[this.dt_.getColumnId(j)].kind == rhizo.meta.Kind.CATEGORY) {
+        model[this.dt_.getColumnId(j)] = this.parseSingleCategory_(value);
+      } else {
+        model[this.dt_.getColumnId(j)] = value;
+      }
+
+    }
+    models.push(model);
+  }
+  return models;
+};
+
+rhizo.gviz.Initializer.prototype.createDefaultRenderer_ =
+    function(metamodel, models) {
+  return new rhizo.autorender.AR(metamodel,
+                                 models,
+                                 this.options_.arDefaults,
+                                 this.options_.arNumFields);
+  // return new rhizo.gviz.DebugRenderer(this.dt_);
+};
+
+rhizo.gviz.DebugRenderer = function(dataTable) {
+  this.dt_ = dataTable;
+};
+
+rhizo.gviz.DebugRenderer.prototype.render = function(model) {
+  var div = $("<div />");
+  for (var j = 0, clen = this.dt_.getNumberOfColumns(); j < clen; j++) {
+    div.append("<p>" + model[this.dt_.getColumnId(j)] + "</p>");
+  }
+  return div;
+};
