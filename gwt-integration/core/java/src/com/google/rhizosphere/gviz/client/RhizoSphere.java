00bb3571ad5387fdc0346063296c117b2e72b780
dinoderek <none@none>
2010-10-29 18:04:13 +0100

    GWT side renderer

--- gwt-integration/core/java/src/com/google/rhizosphere/gviz/client/RhizoSphere.java
+++ gwt-integration/core/java/src/com/google/rhizosphere/gviz/client/RhizoSphere.java
@@ -24,11 +24,32 @@ public class RhizoSphere extends Visualization<RhizoSphere.Options> {
 		protected Options() {
 			super();
 		}
-		// TODO add the renderer option
+
+		/**
+		 * Sets the renderer for the model elements of Rhizosphere. If no Renderer is set, the automatic 
+		 * renderer will be used.
+		 */
+		public final native void setRenderer(Renderer renderer) /*-{
+			this.renderer = {};
+			this.renderer.render = function(pars) {
+				return @com.google.rhizosphere.gviz.client.RhizoSphere.Options::onRender(Lcom/google/rhizosphere/gviz/client/RhizoSphere$Renderer;Lcom/google/gwt/core/client/JavaScriptObject;)
+			    (renderer, pars);
+			};
+		}-*/;
+
+		/**
+		 * This static callback is needed to circumvent JSNI limitations: no instance fields in JSO
+		 * objects, Java object passed to JS are opaque.
+		 */
+		public static String onRender(Renderer renderer, JavaScriptObject params) {
+			Properties props = params.cast();
+			String result = renderer.render(props);
+			return result;
+		}
 	}
 
 	/**
-	 * A RhizoSphere renderer. A RhizoSphere renderer takes a model object and returns an HTML string
+	 * A RhizoSphere renderer that takes a model object and returns an HTML string
 	 * representing it in an appropriate way.
 	 * <p>
 	 * The Renderer will receive an object from RhizoSphere containing a property for each column of
@@ -37,25 +58,12 @@ public class RhizoSphere extends Visualization<RhizoSphere.Options> {
 	 * <p>
 	 * The Renderer will build an HTML representation of the model object and return it as a String
 	 */
-	public static interface Renderer {
-		String render(Properties properties);
-	}
-
-	/**
-	 * A simple wrapper around a Renderer that casts the JSO to a Properties and then delegates the
-	 * actual rendering to the wrapped Renderer instance.
-	 */
-	private static class JsoRenderer {
-		private final Renderer delegate;
-
-		public JsoRenderer(Renderer delegate) {
-			super();
-			this.delegate = delegate;
-		}
+	public static abstract class Renderer {
+		protected abstract String doRender(Properties properties);
 
 		public String render(JavaScriptObject jso) {
 			Properties props = jso.cast();
-			return delegate.render(props);
+			return doRender(props);
 		}
 	}
 
