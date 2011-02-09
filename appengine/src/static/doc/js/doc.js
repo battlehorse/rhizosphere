39a98fe91fdac00f0de20205975d2b75f2fbcbde
Riccardo Govoni <battlehorse@google.com>
2011-02-09 16:51:19 +0000

    Improving TOC autoscrolling to also execute on page load.

--- appengine/src/static/doc/js/doc.js
+++ appengine/src/static/doc/js/doc.js
@@ -18,11 +18,18 @@ function findCurrentDoc(el_id) {
   }
 }
 
+/**
+ * Ensures the TOC will always remain visible on screen even when scrolling
+ * through long documentation pages.
+ */
 function trackScrollingForToc(toc_id) {
   var toc = document.getElementById(toc_id);
   var tocTop = toc.offsetTop;
+
+  // Keep the TOC attached to the main window scrolling if there is not enough
+  // space to fully display it.
   if (toc.offsetHeight < window.innerHeight) {
-    window.onscroll = function() {
+     var toc_onscroll = function() {
       var scrollTop = document.body.scrollTop || document.documentElement.scrollTop;
       var toc = document.getElementById(toc_id);
       if (scrollTop >= tocTop) {
@@ -32,6 +39,8 @@ function trackScrollingForToc(toc_id) {
         toc.style.position = 'relative';
         toc.style.top = '';
       }
-    }
+    };
+    window.onscroll = toc_onscroll;
+    toc_onscroll();
   }
 }
